36f1772f15235315b1da6118490e69739363ee1f
Pindi Shiva Kalyan <129607718+gitpsk03@users.noreply.github.com>
2024-11-21 15:52:24 +0530

    Delete src/components directory

--- src/components/PrivateRoute/index.js
+++
@@ -1,9 +0,0 @@
-import {Navigate} from 'react-router-dom'
-import {useAuth} from 'react-use-auth'
-
-function PrivateRoute({children}) {
-  const auth = useAuth()
-  return auth.user ? children : <Navigate to="/login" />
-}
-
-export default PrivateRoute
